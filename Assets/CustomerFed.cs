a4953bb6a81436343865c9a78e863abeb3223182
vvnzheng <91556534+vvnzheng@users.noreply.github.com>
2023-02-24 13:43:21 -0800

    Deleted projectile gravity, updated destroy for projectile when shot, etc

--- Assets/CustomerFed.cs
+++ Assets/CustomerFed.cs
@@ -10,28 +10,50 @@ public class CustomerFed : MonoBehaviour
 
     public float speed;
     public Transform[] moveSpots;
+    public Transform[] route2;
     private int randomSpot;
+    public int pickRoute;
 
     // Start is called before the first frame update
     void Start()
     {
+        //pickRoute = Random.Range(1, 2);
+        pickRoute = 2;
         money = GameObject.FindGameObjectWithTag("Money").GetComponent<MoneyMoneyMoney>();
         randomSpot = 0;
     }
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        if(pickRoute == 1)
         {
-            if (randomSpot == 0 || randomSpot >= 2)
+            transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
             {
-                if(randomSpot != moveSpots.Length - 1)
+                if (randomSpot == 0 || randomSpot >= 2)
                 {
-                    randomSpot += 1;
+                    if (randomSpot != moveSpots.Length - 1)
+                    {
+                        randomSpot += 1;
+                    }
                 }
+                print("route 1 @ spot");
             }
-            print("@ spot");
+
+        } else if(pickRoute == 2)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, route2[randomSpot].position, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, route2[randomSpot].position) < 0.2f)
+            {
+                if (randomSpot <= 1 || randomSpot >= 3)
+                {
+                    if (randomSpot != route2.Length - 1)
+                    {
+                        randomSpot += 1;
+                    }
+                }
+                print("route 2 @ spot");
+            }
         }
     }
 
@@ -40,8 +62,15 @@ public class CustomerFed : MonoBehaviour
     {
         if (collision.gameObject.layer == 7)
         {
+            Destroy(collision.gameObject);
             money.addScore(4);
-            randomSpot = 2;
+            if(pickRoute == 1)
+            {
+                randomSpot = 2;
+            } else if(pickRoute == 2)
+            {
+                randomSpot = 3;
+            }
         }
 
     }
