42cf7c455bfb19c343bbab904dbee938a1302584
dwild <dwild@ucsc.edu>
2023-02-24 18:22:57 -0800

    Seating Lazer is going well, working on queue stuff

---
+++ Assets/SeatingLaserAssets/SeatQueue.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameCreator.Runtime.Variables;
+
+public class SeatQueue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Lazercomponent;
+   // private LocalNameVariable
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
