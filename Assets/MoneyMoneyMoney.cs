15fb55a8863fa33c0249d84f5acaf304a0108e08
dwild <dwild@ucsc.edu>
2023-03-21 04:57:07 -0700

    WE HAVE A FINAL SCORE

--- Assets/MoneyMoneyMoney.cs
+++ Assets/MoneyMoneyMoney.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MoneyMoneyMoney : MonoBehaviour
 {
     public int moneyScore;
 
     public Text score;
+    public void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+
+
+
+    }
+    public void forthefunny() { 
+        if(SceneManager.GetActiveScene().name == "End Scene")
+        {
+        
+                score = GameObject.Find("Text (Legacy)").GetComponent<Text>();
+            
+            addScore(0);
+        }
+    }
 
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
         moneyScore += scoreToAdd;
+        Debug.Log("amongus" + scoreToAdd);
         score.text = "$" + moneyScore;
     }
 
